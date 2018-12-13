d7868afa600419c01dd14f93684a308ba2a74cf6
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-13 21:15:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(integer)
+  if integer < 2 
+    return false
+  end
+  
+  number = 2..integer-1
+  number.each do |indexs|
+    if integer % indexs == 0
+      return false
+    end
+  end
+  
+  return true
+end
